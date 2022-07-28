89ca26d7cb5f8a3836a5e96473e1dab581d2635e
tuzoep <sas.peter.hu@gmail.com>
2022-07-28 23:24:54 +0200

    Add requirements.txt, tiny hanges

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField, URLField
